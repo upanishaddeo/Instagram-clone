bab105b6842446d8fb32ad5e8e357f8d20192325
upanishaddeo <66788596+upanishaddeo@users.noreply.github.com>
2020-10-14 11:57:07 +0530

    commit 2
    deploying the build react app

---
+++ build/precache-manifest.459c88ede8b69ed8f9cd93854cc48ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba2c198d0ca63ecdaecf0b663542a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2856d4b7f3fdc963e2d",
+    "url": "/static/css/main.13c97b07.chunk.css"
+  },
+  {
+    "revision": "7c3caa7c00f0c995a38b",
+    "url": "/static/js/2.cf117a2f.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.cf117a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2856d4b7f3fdc963e2d",
+    "url": "/static/js/main.33d6a709.chunk.js"
+  },
+  {
+    "revision": "ef2a7bc0ce8a6f99131c",
+    "url": "/static/js/runtime-main.59c43044.js"
+  }
+]);
